963ea79931b71fded8b985b4b18f551703a89ce9
Gael Hopp <galougaga@gmail.com>
2016-08-12 10:28:18 +0200

    add pie example and import amchart js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'amChartsDirective'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the histoTennisApp
  */
 angular.module('histoTennisApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -16,10 +16,95 @@ angular.module('histoTennisApp')
     ];
   
 
+var chart;
+var legend;
+var selected;
 
 
+ var types = [{
+  type: "Fossil Energy",
+  percent: 70,
+  color: "#ff9e01",
+  subs: [{
+    type: "Oil",
+    percent: 15
+  }, {
+    type: "Coal",
+    percent: 35
+  }, {
+    type: "Nuclear",
+    percent: 20
+  }]
+}, {
+  type: "Green Energy",
+  percent: 30,
+  color: "#b0de09",
+  subs: [{
+    type: "Hydro",
+    percent: 15
+  }, {
+    type: "Wind",
+    percent: 10
+  }, {
+    type: "Other",
+    percent: 5
+  }]
+}];
 
 
+ function generateChartData() {
+  var chartData = [];
+  for (var i = 0; i < types.length; i++) {
+    if (i == selected) {
+      for (var x = 0; x < types[i].subs.length; x++) {
+        chartData.push({
+          type: types[i].subs[x].type,
+          percent: types[i].subs[x].percent,
+          color: types[i].color,
+          pulled: true
+        });
+      }
+    } else {
+      chartData.push({
+        type: types[i].type,
+        percent: types[i].percent,
+        color: types[i].color,
+        id: i
+      });
+    }
+  }
+  return chartData;
+}
+
+
+$scope.amChartOptions = {
+  type: "pie",
+  data: generateChartData(),
+  labelText: "[[title]]: [[value]]",
+  balloonText: "[[title]]: [[value]]",
+  titleField: "type",
+  valueField: "percent",
+  outlineColor: "#FFFFFF",
+  outlineAlpha: 0.8,
+  outlineThickness: 2,
+  colorField: "color",
+  pulledField: "pulled",
+  titles: [{
+    text: "Click a slice to see the details"
+  }],"listeners": [{
+    "event": "clickSlice",
+    "method": function(event) {
+      var chart = event.chart;
+      if (event.dataItem.dataContext.id != undefined) {
+        selected = event.dataItem.dataContext.id;
+      } else {
+        selected = undefined;
+      }
+      chart.dataProvider = generateChartData();
+      chart.validateData();
+    }
+  }]
+} 
 
 
   });
