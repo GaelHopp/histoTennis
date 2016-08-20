2140902fbd8b4aca7c781c6ace1c4219df0a33e4
Gael Hopp <galougaga@gmail.com>
2016-08-20 14:30:54 +0200

    refactoring + supress useless comments + comment test button in html

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,7 +17,11 @@ angular.module('histoTennisApp')
   
 
 
-$scope.amChartOptionsTotalVictories = generateTotalVictoriesData();
+$scope.amChartOptionsTotalVictories = generateTotalVictoriesData(); // datas for total victories
+
+/*
+Construct JSon datas with good format for total victories
+*/
 
 function constructTypesForTotalVictories(){
 
@@ -55,47 +59,75 @@ return types;
 
 }
 
+/*
+Generate total victories datas for chart
+*/
+
 function generateTotalVictoriesData(){
 
- var types = [{
-  type: "Fossil Energy",
-  percent: 70,
-  color: "#ff9e01",
-  subs: [{
-    type: "Oil",
-    percent: 15
-  }, {
-    type: "Coal",
-    percent: 35
-  }, {
-    type: "Nuclear",
-    percent: 20
-  }]
-}, {
-  type: "Green Energy",
-  percent: 30,
-  color: "#b0de09",
-  subs: [{
-    type: "Hydro",
-    percent: 15
-  }, {
-    type: "Wind",
-    percent: 10
-  }, {
-    type: "Other",
-    percent: 5
-  }]
-}];
-
 var selected;
-var title = "Matches";
+var title = "Victoires totales";
 return generateChartOptions(constructTypesForTotalVictories(), title);
 
   }
 
 
-$scope.testJsonData = "noOK";
 
+
+
+/*
+
+#################################### GENERIC FUNCTIONS TO GENERATE CHARTS ##################################
+
+*/
+
+
+
+
+/*
+Generic function to generate datas for pie chart
+*/
+
+function generateChartOptions(datas, title){ 
+
+var selected;
+
+var amChartOptions = {
+  type: "pie",
+  data: generateChartData(datas, selected),
+  labelText: "[[title]]: [[value]]",
+  balloonText: "[[title]]: [[value]]",
+  titleField: "type",
+  valueField: "percent",
+  outlineColor: "#FFFFFF",
+  outlineAlpha: 0.8,
+  outlineThickness: 5,
+  colorField: "color",
+  pulledField: "pulled",
+  titles: [{
+    text: title
+  }],"listeners": [{
+    "event": "clickSlice",
+    "method": function(event) {
+      var chart = event.chart;
+      if (event.dataItem.dataContext.id != undefined) {
+        selected = event.dataItem.dataContext.id;
+      } else {
+        selected = undefined;
+      }
+      chart.dataProvider = generateChartData(datas, selected);
+      chart.validateData();
+    }
+  }]
+} 
+
+return amChartOptions;
+
+}
+
+/*
+Generate function to generate datas with sub types
+*/
 
  function generateChartData(types, selected) {
   var chartData = [];
@@ -121,46 +153,7 @@ $scope.testJsonData = "noOK";
   return chartData;
 }
 
-function generateChartOptions(datas, title){ 
 
-var selected;
-
-var amChartOptions = {
-  type: "pie",
-  data: generateChartData(datas, selected),
-  labelText: "[[title]]: [[value]]",
-  balloonText: "[[title]]: [[value]]",
-  titleField: "type",
-  valueField: "percent",
-  outlineColor: "#FFFFFF",
-  outlineAlpha: 0.8,
-  outlineThickness: 5,
-  colorField: "color",
-  pulledField: "pulled",
-  titles: [{
-    text: title
-  }],"listeners": [{
-    "event": "clickSlice",
-    "method": function(event) {
-      var chart = event.chart;
-      console.log("FLAG1 "+event.dataItem.dataContext.id);
-      if (event.dataItem.dataContext.id != undefined) {
-        selected = event.dataItem.dataContext.id;
-        console.log("boucle1");
-      } else {
-        selected = undefined;
-        console.log("boucle2");
-      }
-      console.log("FLAG2 "+event.dataItem.dataContext.id);
-      chart.dataProvider = generateChartData(datas, selected);
-      chart.validateData();
-    }
-  }]
-} 
-
-return amChartOptions;
-
-}
 
  $scope.testJson = function(){
   $http.get('http://localhost:8888/histoTennisBack/api/json').
