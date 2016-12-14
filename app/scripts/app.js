30d87b895037fa9b34dad81d1a27eaa847bd1741
Gael Hopp <galougaga@gmail.com>
2016-12-14 10:42:53 +0100

    add dependencies + construct data for save match

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'amChartsDirective'
+    'amChartsDirective',
+    'ngMaterial'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/histo.js
+++ app/scripts/controllers/histo.js
@@ -37,6 +37,11 @@ angular.module('histoTennisApp')
     	$scope.set3TB = false;
     	$scope.is3sets = false;
     	$scope.scoreCorrect = false;
+
+    	$scope.matchToSave = {
+    		indoorMatch : true
+    	};
+
     }
 
 
@@ -51,10 +56,18 @@ angular.module('histoTennisApp')
     		if($scope.is3sets === true){
     			correct = $scope.checkSet3($scope.pointsSet3Player1, $scope.pointsSet3Player2);
     		}else{
+    			$scope.pointsSet3Player1TB = "";
+    			$scope.pointsSet3Player2TB = "";
+    			$scope.pointsSet3Player1 = "";
+    			$scope.pointsSet3Player2 = "";
     			correct = true;
     		}
     	}else{
     		$scope.is3sets = false;
+    		$scope.pointsSet3Player1TB = "";
+    		$scope.pointsSet3Player2TB = "";
+    		$scope.pointsSet3Player1 = "";
+    		$scope.pointsSet3Player2 = "";
     	}
 
     	$scope.scoreCorrect = correct;
@@ -245,6 +258,195 @@ angular.module('histoTennisApp')
 			$scope.is3sets = true;
 		}
 	};
+
+
+	$scope.constructMatchToSave = function(pointsSet1Player1, pointsSet1Player2, pointsSet2Player1, pointsSet2Player2, pointsSet3Player1, pointsSet3Player2, pointsSet1Player1TB, pointsSet1Player2TB, pointsSet2Player1TB, pointsSet2Player2TB, pointsSet3Player1TB, pointsSet3Player2TB){
+		
+		$scope.matchToSave.dateMatch = $scope.dateMatchToSave;
+
+		if(pointsSet1Player1 > pointsSet1Player2){
+			if(pointsSet2Player1 > pointsSet2Player2){
+				$scope.matchToSave.idWinner = 1;
+				$scope.matchToSave.idLoser = 2;
+			}else{
+				if(pointsSet3Player1 > pointsSet3Player2){
+					$scope.matchToSave.idWinner = 1;
+				$scope.matchToSave.idLoser = 2;
+				}else{
+					$scope.matchToSave.idWinner = 2;
+				$scope.matchToSave.idLoser = 1;
+				}
+			}
+
+		}else{
+			if(pointsSet2Player1 < pointsSet2Player2){
+				$scope.matchToSave.idWinner = 2;
+				$scope.matchToSave.idLoser = 1;
+			}else{
+				if(pointsSet3Player1 > pointsSet3Player2){
+					$scope.matchToSave.idWinner = 1;
+				$scope.matchToSave.idLoser = 2;
+				}else{
+					$scope.matchToSave.idWinner = 2;
+				$scope.matchToSave.idLoser = 1;
+				}
+			}
+		}
+
+
+		
+			
+			var tieBreakLoserPointsSet1 = null;
+			var tieBreakLoserPointsSet2 = null;
+			var tieBreakLoserPointsSet3 = null;
+
+			if(pointsSet1Player1 === 7 && pointsSet1Player2 === 6){
+				tieBreakLoserPointsSet1 = pointsSet1Player2TB;
+			}
+			if(pointsSet1Player1 === 6 && pointsSet1Player2 === 7){
+				tieBreakLoserPointsSet1 = pointsSet1Player1TB;
+			}
+
+			if(pointsSet2Player1 === 7 && pointsSet2Player2 === 6){
+				tieBreakLoserPointsSet2 = pointsSet2Player2TB;
+			}
+			if(pointsSet2Player1 === 6 && pointsSet2Player2 === 7){
+				tieBreakLoserPointsSet2 = pointsSet2Player1TB;
+			}
+
+			if(pointsSet3Player1 != null){
+
+				if(pointsSet3Player1 === 7 && pointsSet3Player2 === 6){
+					tieBreakLoserPointsSet3 = pointsSet3Player2TB;
+				}
+				if(pointsSet3Player1 === 6 && pointsSet3Player2 === 7){
+					tieBreakLoserPointsSet3 = pointsSet3Player1TB;
+				}
+
+			}
+
+
+			if($scope.matchToSave.idWinner === 1){
+
+				$scope.matchToSave.sets = [{
+					winnerGames : $scope.pointsSet1Player1,
+					loserGames : $scope.pointsSet1Player2,
+					superTieBreak : 0,
+					tiebreakLoserPoints : tieBreakLoserPointsSet1
+					},
+					{
+					winnerGames : $scope.pointsSet2Player1,
+					loserGames : $scope.pointsSet2Player2,
+					superTieBreak : 0,
+					tiebreakLoserPoints : tieBreakLoserPointsSet2
+					}
+				];
+
+				if(pointsSet3Player1 != null){
+					var isSTB = 0;
+					if(pointsSet3Player1 > 9 || pointsSet3Player2 > 9){
+						isSTB = 1;
+					}
+
+					$scope.matchToSave.sets[2] = {
+						winnerGames : $scope.pointsSet3Player1,
+						loserGames : $scope.pointsSet3Player2,
+						superTieBreak : isSTB,
+						tiebreakLoserPoints : tieBreakLoserPointsSet3
+					};
+
+				}
+		}
+
+
+		if($scope.matchToSave.idWinner === 2){
+
+				$scope.matchToSave.sets = [{
+					winnerGames : $scope.pointsSet1Player2,
+					loserGames : $scope.pointsSet1Player1,
+					superTieBreak : 0,
+					tiebreakLoserPoints : tieBreakLoserPointsSet1
+					},
+					{
+					winnerGames : $scope.pointsSet2Player2,
+					loserGames : $scope.pointsSet2Player1,
+					superTieBreak : 0,
+					tiebreakLoserPoints : tieBreakLoserPointsSet2
+					}
+				];
+
+				if(pointsSet3Player1 != null){
+					var isSTB = 0;
+					if(pointsSet3Player1 > 9 || pointsSet3Player2 > 9){
+						isSTB = 1;
+					}
+
+					$scope.matchToSave.sets[2] = {
+						winnerGames : $scope.pointsSet3Player2,
+						loserGames : $scope.pointsSet3Player1,
+						superTieBreak : isSTB,
+						tiebreakLoserPoints : tieBreakLoserPointsSet3
+					};
+
+				}
+		}
+		
+	};
+
+	
+	$scope.save = function(){
+		var pointsSet3Player1 = null;
+		var pointsSet3Player2 = null;
+		var pointsSet3Player1TB = null;
+		var pointsSet3Player2TB = null;
+
+		var pointsSet1Player1TB = null;
+		var pointsSet1Player2TB = null;
+		var pointsSet2Player1TB = null;
+		var pointsSet2Player2TB = null;
+
+		var pointsSet1Player1 = parseInt($scope.pointsSet1Player1);
+		var pointsSet1Player2 = parseInt($scope.pointsSet1Player2);
+		var pointsSet2Player1 = parseInt($scope.pointsSet2Player1);
+		var pointsSet2Player2 = parseInt($scope.pointsSet2Player2);
+
+		if($scope.pointsSet1Player1TB != "" && isNaN($scope.pointsSet1Player1TB) === false){
+			pointsSet1Player1TB = parseInt($scope.pointsSet1Player1TB);
+		}
+
+		if($scope.pointsSet1Player2TB != "" && isNaN($scope.pointsSet1Player2TB) === false){
+			pointsSet1Player2TB = parseInt($scope.pointsSet1Player2TB);
+		}
+
+		if($scope.pointsSet2Player1TB != "" && isNaN($scope.pointsSet2Player1TB) === false){
+			pointsSet2Player1TB = parseInt($scope.pointsSet2Player1TB);
+		}
+
+		if($scope.pointsSet2Player2TB != "" && isNaN($scope.pointsSet2Player2TB) === false){
+			pointsSet2Player2TB = parseInt($scope.pointsSet2Player2TB);
+		}
+
+		if($scope.pointsSet3Player1 != "" && isNaN($scope.pointsSet3Player1) === false){
+			pointsSet3Player1 = parseInt($scope.pointsSet3Player1);
+		}
+
+		if($scope.pointsSet3Player2 != "" && isNaN($scope.pointsSet3Player2) === false){
+			pointsSet3Player2 = parseInt($scope.pointsSet3Player2);
+		}
+
+		if($scope.pointsSet3Player1TB != "" && isNaN($scope.pointsSet3Player1TB) === false){
+			pointsSet3Player1TB = parseInt($scope.pointsSet3Player1TB);
+		}
+
+		if($scope.pointsSet3Player2TB != "" && isNaN($scope.pointsSet3Player2TB) === false){
+			pointsSet3Player2TB = parseInt($scope.pointsSet3Player2TB);
+		}
+
+		$scope.constructMatchToSave(pointsSet1Player1, pointsSet1Player2, pointsSet2Player1, pointsSet2Player2, pointsSet3Player1, pointsSet3Player2, pointsSet1Player1TB, pointsSet1Player2TB, pointsSet2Player1TB, pointsSet2Player2TB, pointsSet3Player1TB, pointsSet3Player2TB);
+		console.log($scope.matchToSave);
+
+	}
+
 
 	$scope.init();
 
