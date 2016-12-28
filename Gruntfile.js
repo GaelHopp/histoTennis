39b49f768110fed95035c29184a7a49271722af7
Gael Hopp <galougaga@gmail.com>
2016-12-28 13:19:26 +0100

    add dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,8 +9,11 @@
 
 module.exports = function (grunt) {
 
+  var pkg = require('./package.json');
+
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
+  grunt.loadNpmTasks('grunt-build-control');
 
   // Automatically load required Grunt tasks
   require('jit-grunt')(grunt, {
@@ -416,6 +419,34 @@ module.exports = function (grunt) {
         'svgmin'
       ]
     },
+
+     buildcontrol: {
+    options: {
+      dir: 'dist',
+      commit: true,
+      push: true,
+      message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+    },
+    pages: {
+      options: {
+        remote: 'git@github.com:GaelHopp/histoTennis.git',
+        branch: 'gh-pages'
+      }
+    },
+    heroku: {
+      options: {
+        remote: 'git@heroku.com:example-heroku-webapp-1988.git',
+        branch: 'master',
+        tag: pkg.version
+      }
+    },
+    local: {
+      options: {
+        remote: '../',
+        branch: 'build'
+      }
+    }
+  },
 
     // Test settings
     karma: {
